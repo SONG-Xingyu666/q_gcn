54c1f7ff29c582d4c51bd495b3f401994923484e
SONG-Xingyu666 <songxingyu0429@gmail.com>
2024-05-08 19:08:15 +0900

    linux

--- model.py
+++ model.py
@@ -1,1 +1,10 @@
 import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import numpy as np
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+class GraphConvolution(nn.Module):
+    def __init__(self) -> None:
+        super().__init__()
