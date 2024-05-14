c6f4acca309f8ef4666a40c470cbb52058b82d0f
SONG-Xingyu666 <songxingyu0429@gmail.com>
2024-05-14 17:40:13 +0900

    linux

---
+++ build/lib/processor/io.py
@@ -0,0 +1,24 @@
+import sys 
+import argparse
+import yaml
+import numpy as np
+
+import torch 
+import torch.nn as nn
+
+
+
+
+my_project/
+│
+├── processor/
+│   ├── __init__.py
+│   └── io.py
+│
+└── torchlight/
+    ├── setup.py
+    └── torchlight
+        ├── __init__.py
+        ├── io.py
+        └── gpu.py
+        
--- processor/io.py
+++ processor/io.py
@@ -6,5 +6,6 @@ import numpy as np
 import torch 
 import torch.nn as nn
 
-from torchlight import str2bool
 import torchlight
+
+        
