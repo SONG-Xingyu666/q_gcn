0c84e67395f1122e026dc715bb40bb9d38eae0c2
SONG-Xingyu666 <songxingyu0429@gmail.com>
2024-05-16 18:08:11 +0900

    linux

--- processor/io.py
+++ processor/io.py
@@ -8,4 +8,34 @@ import torch.nn as nn
 
 import torchlight
 
+class IO():
+    def __init__(self, argv=None):
+        
+        self.load_arg(argv)
+        
+    def load_arg(self, argv=None):
+        pass
+    
+    def init_environment(self):
+        pass
+    
+    def load_model(self):
+        pass
+    
+    def load_weights(self):
+        pass
+    
+    def gpu(self):
+        pass
+    
+    def start(self):
+        pass
+    
+    def start(self):
+        pass
+    
+    @staticmethod
+    def get_paser(add_help=False):
+        
+        parser = argparse.ArgumentParser(add_help=add_help, description='IO Processor')
         
